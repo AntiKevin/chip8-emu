6053d970e49d01e3b6fefbadad9636a502b89bd0
antikevin <kevin_3467@yahoo.com>
2024-12-15 19:47:46 -0300

    feat: :sparkles: Refatora a estrutura de escopo  da CPU e inicia  a função executeOpcode

--- emulator/cpu.go
+++ emulator/cpu.go
@@ -1,6 +1,8 @@
 package emulator
 
-type CPU struct {
+import "fmt"
+
+type cpu struct {
 	V          [16]uint8  // 16 registradores de 8 bits (V0 a VF)
 	I          uint16     // Registrador de endereço (16 bits)
 	PC         uint16     // Program Counter (PC), aponta para a próxima instrução
@@ -10,8 +12,8 @@ type CPU struct {
 	SoundTimer uint8      // Timer de som, emite som enquanto for maior que 0
 }
 
-func NewCPU() *CPU {
-	return &CPU{
+func newCPU() *cpu {
+	return &cpu{
 		V:          [16]uint8{},
 		I:          0,
 		PC:         0x200,
@@ -23,7 +25,7 @@ func NewCPU() *CPU {
 }
 
 // FetchOpcode busca e retorna o opcode da memória e incrementa o PC
-func (cpu *CPU) FetchOpcode(memory []uint8) uint16 {
+func (cpu *cpu) fetchOpcode(memory []uint8) uint16 {
 	highByte := uint16(memory[cpu.PC])
 	lowByte := uint16(memory[cpu.PC+1])
 	opcode := (highByte << 8) | lowByte
@@ -31,4 +33,26 @@ func (cpu *CPU) FetchOpcode(memory []uint8) uint16 {
 	cpu.PC += 2
 
 	return opcode
+}
+
+// ExecuteOpcode executa a instrução baseada no opcode
+func (cpu *cpu) executeOpcode(opcode uint16, memory []uint8) {
+	switch opcode & 0xF000 {
+	case 0x0000:
+		if opcode == 0x00E0 {
+			// Limpar a tela
+		} else if opcode == 0x00EE {
+			// Retornar de uma sub-rotina
+		}
+	case 0x1000:
+		// Salto para o endereço NNN
+		cpu.PC = opcode & 0x0FFF
+	case 0x2000:
+		// Chamar sub-rotina no endereço NNN
+		cpu.Stack[cpu.SP] = cpu.PC
+		cpu.SP++
+		cpu.PC = opcode & 0x0FFF
+	default:
+		fmt.Printf("Opcode desconhecido: 0x%X\n", opcode)
+	}
 }

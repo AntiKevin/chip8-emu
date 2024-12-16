3e1ffec52c260ac3ed457e8ed6a8e1b8d4cfca80
antikevin <kevin_3467@yahoo.com>
2024-12-15 21:47:31 -0300

    feat: :sparkles: Atualiza função RunEmulator para carregar ROM e iniciar emulador

--- emulator/main.go
+++ emulator/main.go
@@ -1,10 +1,19 @@
 package emulator
 
-func RunEmulator(romTest string) {
+func RunEmulator(romPath string) {
+	println("Iniciando emulador...")
+
 	cpu := newCPU()
 	memory := newMemory()
-	opcode := cpu.fetchOpcode(memory.ram[:])
+	opcode := uint16(0)
 
-	println("opcode:", opcode)
-	println("romTest:", romTest)
+	println("Carregando ROM...")
+	memory.LoadROM(cpu, romPath, memory.ram[:])
+	println("ROM carregada com sucesso!")
+
+	print("Iniciando loop principal...")
+	for {
+		opcode = cpu.fetchOpcode(memory.ram[:])
+		cpu.executeOpcode(opcode, memory.ram[:])
+	}
 }
